f1bd32a77dcac540476696c652418e080711e691
robbrotherton <robert.brotherton@gmail.com>
2022-08-15 10:29:58 -0400

    refactor trail start particles

--- R/make_trails.R
+++ R/make_trails.R
@@ -1,8 +1,7 @@
 #' Generate flowfield trails
 #'
-#' @param flowfield_df
+#' @param flowfield
 #' @param particles
-#' @param start_positions
 #' @param max_steps
 #' @param step_length
 #' @param direction
@@ -12,44 +11,23 @@
 #' @export
 #'
 #' @examples
-make_trails <- function(flowfield_df,
-                        start_positions,
-                        particles = 100,
+make_trails <- function(flowfield,
+                        particles = particles_poisson(10, lims(flowfield)),
                         max_steps = 1,
                         step_length = .01,
                         direction = c("both", "forward", "backward"),
                         dtest = 0) {
 
-  # start_positions <- match.arg(start_positions,
-  #                              choices = c("grid",
-  #                                          "runif",
-  #                                          "poisson"))
 
   direction <- match.arg(direction)
 
-  ff_width <- max(flowfield_df$x) # - min(flowfield_df$x)
-  ff_height <- max(flowfield_df$y) # - min(flowfield_df$y)
+  ff_width <- max(flowfield$x) # - min(flowfield_df$x)
+  ff_height <- max(flowfield$y) # - min(flowfield_df$y)
   max_steps <- as.integer(ff_width * max_steps)
   step_length <- ff_width * step_length
 
-  if (is.data.frame(start_positions)) {
-    particles <- start_positions
-  } else if (start_positions == "poisson")  {
-    particles <- pack_circles_cpp(ff_width, ff_height,
-                                  max_circles = particles,
-                                  r = ff_width / sqrt(particles) / 2)
 
-  } else if (start_positions == "grid") {
-    particles <- ambient::long_grid(x = seq(1, ff_width, length.out=sqrt(particles)),
-                                    y = seq(1, ff_height, length.out=sqrt(particles)))
-  } else if (start_positions == "runif") {
-    particles <- data.frame(x = runif(particles, min = 1, max = ff_width),
-                            y = runif(particles, min = 1, max = ff_height))
-  } else {
-    stop("start positions not specified correctly")
-  }
-
-  make_trails_rcpp(field_df = flowfield_df,
+  make_trails_rcpp(field_df = flowfield,
                    particles = particles,
                    max_steps = max_steps,
                    step_length = step_length,
@@ -57,3 +35,25 @@ make_trails <- function(flowfield_df,
                    dtest = dtest)
 
 }
+
+
+particles_poisson <- function(n, limits) {
+  pack_circles_cpp(limits[1], limits[2],
+                   max_circles = n,
+                   r = limits[1] / sqrt(n) / 2)
+}
+
+particles_grid <- function(n, limits) {
+  ambient::long_grid(x = seq(1, limits[1], length.out=sqrt(n)),
+                     y = seq(1, limits[2], length.out=sqrt(n)))
+}
+
+particles_unif <- function(n, limits) {
+  data.frame(x = runif(n, min = 1, max = limits[1]),
+             y = runif(n, min = 1, max = limits[2]))
+}
+
+lims <- function(df) {
+  c(max(df$x), max(df$y))
+}
+
