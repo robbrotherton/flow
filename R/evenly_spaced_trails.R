b4738205669b05336d667924c0b311f45be6e82a
robbrotherton <robert.brotherton@gmail.com>
2022-08-22 08:52:48 -0400

    clean up code

--- R/evenly_spaced_trails.R
+++ R/evenly_spaced_trails.R
@@ -1,6 +1,6 @@
 # https://web.cs.ucdavis.edu/~ma/SIGGRAPH02/course23/notes/papers/Jobard.pdf
 
-#' Title
+#' Make evenly-spaced trails on a flowfield
 #'
 #' @param flowfield_df A data.frame containing rows with x and y coordinates and
 #'   a corresponding angle.
@@ -13,10 +13,11 @@
 #'   hits the boundary of the flowfield or gets too close to another line
 #'   first.)
 #'
-#' @return
+#' @return A data frame with a 'group' column identifying each individual trail,
+#'   x and y coordinates, and the grid angle associated with each coordinate
 #' @export
 #'
-#' @examples
+#' @examples make_flowfield() |> make_trails_even() |> draw_trails()
 make_trails_even <- function(flowfield_df, max_steps = 100, dsep = .05, dtest = .5) {
 
   # need some properties of the flowfield and parameters as variables
@@ -65,24 +66,14 @@ make_trails_even <- function(flowfield_df, max_steps = 100, dsep = .05, dtest = 
       starting_x <- seeds$x[i]
       starting_y <- seeds$y[i]
 
-      # check if the point is valid
-
-      # valid <- check_neighbors(starting_x, starting_y,
-      #                          existing_points = dplyr::bind_rows(lines),
-      #                          dtest = dtest) &
-      #          inbounds(starting_x, starting_y, width, height)
-
       valid <- check_valid(starting_x, starting_y,
                            width, height,
                            existing_points = dplyr::bind_rows(lines),
                            dtest = dtest)
 
-      # valid <- !query_grid(grid, starting_x, starting_y, width, height, dsep, dtest)
-
       if(valid) {
 
         line_number <- line_number + 1
-        # print(line_number)
 
         new_line <- make_trail(starting_x,
                                starting_y,
@@ -91,14 +82,11 @@ make_trails_even <- function(flowfield_df, max_steps = 100, dsep = .05, dtest = 
                                step_length = step_length,
                                dtest = dtest)
 
-        # grid <- add_to_grid(grid, new_line$x, new_line$y, width, dsep)
         lines[[line_number]] <- new_line
 
       }
     }
 
-    # once we tried all seed points for the current queue line, increment the
-    # queue position
     queue_position <- queue_position + 1
 
   }
@@ -114,101 +102,24 @@ inbounds <- function(x, y, w, h) {
   x > 1 & x < w & y > 1 & y < h
 }
 
-# returns TRUE if the nearest neighbor is too close
-# check_neighbor <- function(data, qtree, x, y, test_distance) {
-#   index <- SearchTrees::knnLookup(qtree, x, y, k = 1)
-#   dist <- sqrt((x-data$x[index])^2+(y-data$y[index])^2)
-#   dist < test_distance
-# }
-
 get_seeds <- function(line, dsep) {
-  # take the first line in the queue (the oldest line) as the new current line
 
   n <- nrow(line)
   x <- numeric(n*2)
   y <- numeric(n*2)
-  # now compute all possible seed points; for each row of the df
-  for(i in 1:n) {
 
+  for(i in 1:n) {
     x[i]   <- line$x[i] + dsep * cos(line$a[i] + pi/2)
     x[i+n] <- line$x[i] + dsep * cos(line$a[i] - pi/2)
     y[i]   <- line$y[i] + dsep * sin(line$a[i] + pi/2)
     y[i+n] <- line$y[i] + dsep * sin(line$a[i] - pi/2)
   }
 
-  # all_possible_seeds <- data.frame(x = x, y = y) |>
-  #   dplyr::filter(inbounds(x, y, w, h)) |>
-  #   dplyr::mutate(rand = sample(1:dplyr::n(), dplyr::n())) |>
-  #   dplyr::arrange(rand)
-  #
-  # return(all_possible_seeds)
-
   data.frame(x = x, y = y) |>
     dplyr::mutate(rand = sample(1:dplyr::n(), dplyr::n())) |>
     dplyr::arrange(rand)
+
 }
-
-# create new seedpoints
-# update_current_line <- function(queue, dsep, w, h) {
-#   # take the first line in the queue (the oldest line) as the new current line
-#   line <- queue[[1]]
-#   n <- nrow(line)
-#   x <- numeric()
-#   y <- numeric()
-#   # now compute all possible seed points; for each row of the df
-#   for(i in 1:n) {
-#
-#     x[i]   <- line$x[i] + dsep * cos(line$a[i] + pi/2)
-#     x[i+n] <- line$x[i] + dsep * cos(line$a[i] - pi/2)
-#     y[i]   <- line$y[i] + dsep * sin(line$a[i] + pi/2)
-#     y[i+n] <- line$y[i] + dsep * sin(line$a[i] - pi/2)
-#   }
-#
-#   all_possible_seeds <- data.frame(x = x, y = y) |>
-#     dplyr::filter(inbounds(x, y, w, h)) |>
-#     dplyr::mutate(rand = sample(1:dplyr::n(), dplyr::n())) |>
-#     dplyr::arrange(rand)
-#
-#   return(all_possible_seeds)
-# }
-
-
-# w <- 50
-# h <- 50
-# dsep <- w*.05 #w*.1 # can be .1, .05
-# dtest <- dsep*.9
-#
-# df <- make_flowfield(w, h)
-# draw_field(df)
-#
-# dft <- make_trails(df, start_positions = "grid")
-# dfx <- make_trail_x(df)
-#
-# draw_trails(dfx)
-#
-#
-# ffdf <- make_flowfield(50, 50, angle = 1, octaves = 1, frequency = .01, plot = FALSE)
-# #
-# t <- make_trail(ffdf) |>
-#   tibble::rownames_to_column("point")
-#
-# ggplot2::ggplot(t, ggplot2::aes(x, y, group = line)) +
-#   ggplot2::geom_path() +
-#   ggplot2::coord_fixed()
-
-# pic <- t |>
-#   ggplot2::ggplot() +
-#   ggforce::geom_circle(mapping = ggplot2::aes(x0 = x, y0 = y, r = dtest/2)) +
-#   # geom_point(mapping = aes(x, y)) +
-#   # geom_text(data = t, mapping = aes(x, y, label = group), size = 8, color = 'red') +
-#   ggplot2::geom_path(mapping = ggplot2::aes(x, y, group = line, color = ifelse(line==2, 'red', 'black'))) +
-#   ggplot2::coord_fixed() +
-#   # scale_x_continuous(breaks = 1:10) +
-#   # scale_y_continuous(breaks = 1:10) +
-#   ggplot2::scale_color_identity() +
-#   ggplot2::theme_void() +
-#   NULL
-# pic
 
 
 grid <- function(x, y, width, dsep) {
@@ -216,7 +127,6 @@ grid <- function(x, y, width, dsep) {
   y_ind <- ceiling(y*1/dsep)
 
   grid_ind <- x_ind + (y_ind-1) * width*1/dsep
-
 }
 
 
@@ -270,16 +180,6 @@ grid_indices <- function(x, y, width, height, dsep) {
 
 }
 
-# grid_indices(x = 2, y = 19)
-# edges <- grid_edges(w, h, dsep)
-#
-# z <- tibble(x = c(0,-1,0,1,1, 1, 0,-1,-1),
-#             y = c(0, 1,1,1,0,-1,-1,-1, 0))
-# z[!z$x %in% -1 & !z$y %in% NA,]
-
-# takes tentative new x, y coords, looks up close neighbors from the grid
-# calculates distance to each existing point. Returns T if there's a point < min dist/F
-# could go on to query surrounding cells if no collision is detected...
 query_grid <- function(grid, x, y, width, height, dsep, dtest) {
 
   x0 <- get_index(x, dsep)
