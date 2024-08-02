35e4d13215ba9d65b16161232af083405e30bbc6
robbrotherton <robert.brotherton@gmail.com>
2024-08-02 19:39:32 -0400

    updates documentation

---
+++ R/zzz.R
@@ -0,0 +1,11 @@
+#' flow
+#'
+#' Create a flowfield, populate it with trails, and visualize the result
+#'
+#' @docType package
+#' @author Rob Brotherton <robert.brotherton@gmail.com>
+#' @import Rcpp ambient
+#' @importFrom Rcpp evalCpp
+#' @useDynLib flow
+#' @name flow
+NULL
