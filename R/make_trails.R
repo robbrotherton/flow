11698a50254210f01884df517433c8c7dc2b7903
robbrotherton <robert.brotherton@gmail.com>
2024-08-06 20:09:41 -0400

    updates documentation

--- R/make_trails.R
+++ R/make_trails.R
@@ -1,8 +1,9 @@
 #' Generate flowfield trails
 #'
-#' @param flowfield A data frame, or a list of data frames, containing three columns, x, y, and a: the
-#'   coordinates of the flowfield grid and the angle associated with each grid
-#'   reference.
+#' @param flowfield A data frame, or a list of data frames. Each flowfield data
+#'   frame must contain three columns, x, y, and a: the coordinates of the
+#'   flowfield grid and the angle associated with each grid reference. If a list
+#'   of data frames is supplied, trails will be drawn in alternating order.
 #' @param particles A data frame containing two columns, x and y: the
 #'   coordinates of the starting points (particles) from which trails will be
 #'   generated on the flowfield.
@@ -17,14 +18,16 @@
 #' @param dtest The test distance for terminating a trail when it becomes too
 #'   close to an existing point. Larger values result in sparser trails. A value
 #'   of zero means lines will not be terminated.
+#' @param existing_trails_df A data frame containing previously-drawn trails.
+#'   New trails will avoid collisions with these according to dtest.
 #'
-#' @return
+#' @return A data frame.
 #' @export
 #'
 #' @examples make_flowfield() |> make_trails() |> draw_trails()
 make_trails <- function(flowfield,
                         particles = particles_poisson(100, lims(flowfield)),
-                        max_steps = 20,
+                        max_steps = 100,
                         step_length = 1,
                         direction = c("both", "forward", "backward"),
                         dtest = 0,
@@ -71,9 +74,8 @@ particles_unif <- function(n, limits) {
 }
 
 lims <- function(df) {
-  if ("list" %in% class(df)) {
+  if (is.list(df)) {
     df <- df[[1]]
   }
   c(max(df$x), max(df$y))
 }
-
